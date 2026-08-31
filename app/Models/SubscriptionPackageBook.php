d664fd97e68c65c3e11d678aff6ad3ab058c23b0
devolopzie <hannanaziziemuharyo@smkwikrama.sch.id>
2026-08-31 12:51:58 +0700

    first commit

---
+++ app/Models/SubscriptionPackageBook.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable([
+    'book_id',
+    'subscription_package_id',
+    'expired_date',
+])]
+class SubscriptionPackageBook extends Model
+{
+    public function book() : BelongsTo {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function subscriptionPackage() : BelongsTo {
+        return $this->belongsTo(SubscriptionPackage::class);
+    }
+}
